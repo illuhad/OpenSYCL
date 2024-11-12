a9cae2bf7be059c3ebda7c47dc084b60701a03fd
Jules H. <JuTheDragon@proton.me>
2024-11-12 23:04:40 +0100

    changed the arch add repo script to not do a partial upgrade but instead a full system upgrade as partial upgrades are unsupported.

--- install/scripts/add-hipsycl-repo/archlinux-rolling.sh
+++ install/scripts/add-hipsycl-repo/archlinux-rolling.sh
@@ -7,6 +7,6 @@ echo "Server = http://repo.urz.uni-heidelberg.de/sycl${1}/archlinux/x86_64" >> /
 pacman-key --init
 wget -q -O - http://repo.urz.uni-heidelberg.de/sycl/hipsycl.asc | pacman-key --add -
 pacman-key --lsign-key E967BA09716F870320089583E68CC4B9B2B75080
-pacman -Sy
+pacman -Syu
 
 
